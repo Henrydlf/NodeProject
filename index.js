1771b20b7b10e4d1c4c2bfff147b4dcd0ded360b
Henrydlf <36404566+Henrydlf@users.noreply.github.com>
2019-12-10 11:21:10 +0100

    structure

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.set('view engine', 'ejs');
 
 app.get(
     '/:name', 
-    (req, res) => res.render('Login.ejs', {name: req.params.name})
+    (req, res) => res.render('pages/Login.ejs', {name: req.params.name})
   )
+
+app.get(
+  '/', 
+  (req, res) => res.render('pages/HomePage.ejs', {name: req.params.name})
+)
 
 app.listen(
   app.get('port'), 
